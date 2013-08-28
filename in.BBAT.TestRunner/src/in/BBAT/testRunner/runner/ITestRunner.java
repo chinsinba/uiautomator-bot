e335c9e6a1654afd1176fa175cdc019127d210b4
Syed Mehtab <mehtab.syed@gmail.com>
2013-08-28 20:22:20 +0530

    added testrunner

---
+++ in.BBAT.TestRunner/src/in/BBAT/testRunner/runner/ITestRunner.java
@@ -0,0 +1,9 @@
+package in.BBAT.testRunner.runner;
+
+public interface ITestRunner {
+
+	void execute();
+
+	void abort();
+	
+}
---
+++ in.BBAT.TestRunner/src/in/BBAT/testRunner/runner/TestRunner.java
@@ -0,0 +1,21 @@
+package in.BBAT.testRunner.runner;
+
+import com.android.ddmlib.testrunner.TestIdentifier;
+
+public class TestRunner implements ITestRunner{
+
+	
+	TestIdentifier id;
+	@Override
+	public void execute() {
+		
+	}
+
+	@Override
+	public void abort() {
+		
+	}
+
+	
+	
+}
