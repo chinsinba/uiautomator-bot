604db5a2ab2161051a3a2c67b9529b202dafc72b
Syed Mehtab <mehtab.syed@gmail.com>
2013-09-09 19:43:35 +0530

    added nodes and Ui tree View to developer perspective

---
+++ in.BBAT.presenter/src/in/bbat/presenter/views/developer/NodesView.java
@@ -0,0 +1,26 @@
+package in.bbat.presenter.views.developer;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class NodesView extends ViewPart {
+
+	public static final String ID ="in.BBAT.presenter.developer.nodeDetailView";
+	
+	public NodesView() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
