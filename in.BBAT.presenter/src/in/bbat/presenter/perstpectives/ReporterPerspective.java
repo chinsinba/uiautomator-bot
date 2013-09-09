96e11efe92c25cc940d2aef73587220f5f0e6bb2
Syed Mehtab <mehtab.syed@gmail.com>
2013-09-09 21:54:33 +0530

    perspective extensions created

--- in.BBAT.presenter/src/in/bbat/presenter/perstpectives/ReporterPerspective.java
+++ in.BBAT.presenter/src/in/bbat/presenter/perstpectives/ReporterPerspective.java
@@ -3,9 +3,10 @@ package in.bbat.presenter.perstpectives;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
-public class TesterPerspective implements IPerspectiveFactory {
+public class ReporterPerspective implements IPerspectiveFactory {
 
-	public static final String ID ="in.BBAT.presenter.perspective.tester";
+	
+	public static final String PERSPECTIVE_ID = "in.BBAT.presenter.perspective.reporter";
 	@Override
 	public void createInitialLayout(IPageLayout layout) {
 		layout.setEditorAreaVisible(false);
