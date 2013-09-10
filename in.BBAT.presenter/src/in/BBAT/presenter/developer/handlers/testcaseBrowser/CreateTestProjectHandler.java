6a7aba07f91af0669af193f8758fe7dcae2e1a6f
Syed Mehtab <mehtab.syed@gmail.com>
2013-09-10 15:43:41 +0530

    created testproject command

--- in.BBAT.presenter/src/in/BBAT/presenter/developer/handlers/testcaseBrowser/CreateTestProjectHandler.java
+++ in.BBAT.presenter/src/in/BBAT/presenter/developer/handlers/testcaseBrowser/CreateTestProjectHandler.java
@@ -1,14 +1,13 @@
 package in.BBAT.presenter.developer.handlers.testcaseBrowser;
 
-import in.BBAT.presenter.developer.handlers.BBATHandler;
-
 import org.eclipse.core.commands.ExecutionEvent;
 
-public class CreateTestCaseHandler extends BBATHandler {
+import in.BBAT.presenter.developer.handlers.BBATHandler;
+
+public class CreateTestProjectHandler extends BBATHandler {
 
 	@Override
 	protected Object run(ExecutionEvent event) {
-		
 		return null;
 	}
 
