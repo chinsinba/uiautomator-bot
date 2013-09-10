5de6396dfa7ecb26a3432e02b924a76f640b2fbf
Syed Mehtab <mehtab.syed@gmail.com>
2013-09-10 14:23:28 +0530

    created handlers and commands

---
+++ in.BBAT.presenter/src/in/BBAT/presenter/developer/handlers/BBATHandler.java
@@ -0,0 +1,42 @@
+package in.BBAT.presenter.developer.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandlerListener;
+
+public abstract class BBATHandler extends AbstractHandler {
+
+	@Override
+	public void addHandlerListener(IHandlerListener handlerListener) {
+
+	}
+
+	@Override
+	public void dispose() {
+
+	}
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		return run(event);
+	}
+
+	abstract protected Object run(ExecutionEvent event);
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+	@Override
+	public boolean isHandled() {
+		return true;
+	}
+
+	@Override
+	public void removeHandlerListener(IHandlerListener handlerListener) {
+
+	}
+
+}
---
+++ in.BBAT.presenter/src/in/BBAT/presenter/developer/handlers/testcaseBrowser/CreateTestCaseHandler.java
@@ -0,0 +1,14 @@
+package in.BBAT.presenter.developer.handlers.testcaseBrowser;
+
+import in.BBAT.presenter.developer.handlers.BBATHandler;
+
+import org.eclipse.core.commands.ExecutionEvent;
+
+public class CreateTestCaseHandler extends BBATHandler {
+
+	@Override
+	protected Object run(ExecutionEvent event) {
+		return null;
+	}
+
+}
