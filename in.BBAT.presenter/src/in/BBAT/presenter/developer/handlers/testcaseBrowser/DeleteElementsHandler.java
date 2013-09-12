145a07877fdf274ef09e2f96d00d0c34f94083e7
Syed Mehtab <mehtab.syed@gmail.com>
2013-09-12 21:30:39 +0530

    added delete action in testcase view

---
+++ in.BBAT.presenter/src/in/BBAT/presenter/developer/handlers/testcaseBrowser/DeleteElementsHandler.java
@@ -0,0 +1,20 @@
+package in.BBAT.presenter.developer.handlers.testcaseBrowser;
+
+import in.BBAT.presenter.developer.handlers.BBATHandler;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.IHandler;
+
+public class DeleteElementsHandler extends BBATHandler implements IHandler {
+
+	@Override
+	protected Object run(ExecutionEvent event) {
+		return null;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return super.isEnabled();
+	}
+}
