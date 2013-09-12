192fcbd9f5d70229f66ed88559da07a8e613db3f
Syed Mehtab <mehtab.syed@gmail.com>
2013-09-12 21:40:12 +0530

    Added edit script action to testcase browser

---
+++ in.BBAT.presenter/src/in/BBAT/presenter/developer/handlers/testcaseBrowser/EditScriptHandler.java
@@ -0,0 +1,15 @@
+package in.BBAT.presenter.developer.handlers.testcaseBrowser;
+
+import org.eclipse.core.commands.ExecutionEvent;
+
+import in.BBAT.presenter.developer.handlers.BBATHandler;
+
+public class EditScriptHandler extends BBATHandler {
+
+	@Override
+	protected Object run(ExecutionEvent event) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
