5e3ba25faa5e5a7140458b205e511f37822dacb2
Syed Mehtab <mehtab.syed@gmail.com>
2013-10-12 16:58:38 +0530

    added history perspective

---
+++ in.BBAT.presenter/src/in/bbat/presenter/perstpectives/HistoryPerspective.java
@@ -0,0 +1,15 @@
+package in.bbat.presenter.perstpectives;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class HistoryPerspective implements IPerspectiveFactory {
+
+	public static final String PERSPECTIVE_ID = "in.BBAT.presenter.perspective.history";
+	
+	@Override
+	public void createInitialLayout(IPageLayout layout) {
+		layout.setEditorAreaVisible(false);
+	}
+
+}
