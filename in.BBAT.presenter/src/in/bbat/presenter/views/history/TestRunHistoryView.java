86ce5c645722c8e9427947023dc12fdd92e4f76e
Syed Mehtab <mehtab.syed@gmail.com>
2013-10-12 17:01:42 +0530

    created history testrun view

---
+++ in.BBAT.presenter/src/in/bbat/presenter/views/history/TestRunHistoryView.java
@@ -0,0 +1,35 @@
+package in.bbat.presenter.views.history;
+
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.widgets.Composite;
+
+import in.bbat.presenter.views.BBATViewPart;
+
+public class TestRunHistoryView extends BBATViewPart {
+
+	public static final String ID="in.BBAT.presenter.history.TestRunHistoryView";
+	@Override
+	public void refresh() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public ISelection getSelectedElements() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
