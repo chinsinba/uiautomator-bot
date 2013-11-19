4322da110c96375da364dfb009b7bc2e5b46c488
Syed Mehtab <mehtab.syed@gmail.com>
2013-11-19 22:03:19 +0530

    added busy indicator

--- in.BBAT.presenter/src/in/BBAT/presenter/history/handlers/DeleteTestRunHandler.java
+++ in.BBAT.presenter/src/in/BBAT/presenter/history/handlers/DeleteTestRunHandler.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.apache.log4j.Logger;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.custom.BusyIndicator;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.PlatformUI;
 
 import in.BBAT.abstrakt.presenter.pkg.model.AbstractProjectTree;
@@ -19,18 +21,25 @@ public class DeleteTestRunHandler extends AbstractTestRunBrowserHandler {
 
 	private static final Logger LOG = BBATLogger.getLogger(DeleteTestRunHandler.class.getName());
 	@Override
-	public Object run(ExecutionEvent event, List<?> selectedObjects) {
+	public Object run(ExecutionEvent event,final  List<?> selectedObjects) {
 
 		LOG.info("Delete test run ");
 		if(!MessageDialog.openQuestion(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), "Delete","Do you want to delete ?"))
 			return null;
-		try {
-			for(Object pkgObj :selectedObjects){
-				((TestRunModel)pkgObj).delete();
+		BusyIndicator.showWhile(Display.getDefault(), new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					for(Object pkgObj :selectedObjects){
+						((TestRunModel)pkgObj).delete();
+					}
+				} catch (Exception e) {
+					LOG.error(e);
+				}				
 			}
-		} catch (Exception e) {
-			LOG.error(e);
-		}
+		});
+
 
 		BBATViewPart view = (BBATViewPart) PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().findView(TestRunHistoryView.ID);
 		try {
