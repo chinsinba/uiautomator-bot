ec1bb72d298b3a8bacfe634aaa4b184e418b1de1
mehtab <mehtab.syed@gmail.com>
2013-11-29 12:27:09 +0530

    Added export logs action

---
+++ in.BBAT.presenter/src/in/BBAT/presenter/history/handlers/ExportLogs.java
@@ -0,0 +1,29 @@
+package in.BBAT.presenter.history.handlers;
+
+import java.util.List;
+
+import org.eclipse.core.commands.ExecutionEvent;
+
+import in.BBAT.presenter.developer.handlers.BBATHandler;
+
+public class ExportLogs extends BBATHandler {
+
+	@Override
+	public Object run(ExecutionEvent event, List<?> selectedObjects) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object run(ExecutionEvent event) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEnabled(List<?> object) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
