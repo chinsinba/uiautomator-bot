2de6cd701c9c16c9311813026e2d22714b486388
Syed Mehtab <mehtab.syed@gmail.com>
2013-12-25 12:53:54 +0530

    UFF!!!...feature fixed..

--- in.BBAT.p2.rcpupdate.utils/src/in/bbat/p2/rcpupdate/utils/plugin/Activator.java
+++ in.BBAT.p2.rcpupdate.utils/src/in/bbat/p2/rcpupdate/utils/plugin/Activator.java
@@ -9,7 +9,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.eclipselabs.p2.rcpupdate.utils"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "in.BBAT.p2.rcpupdate.utils"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
