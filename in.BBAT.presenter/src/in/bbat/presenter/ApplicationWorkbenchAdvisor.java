8252f43dd5d2ae0e0b1204c611a161ef0186fcef
mehtab <mehtab.syed@gmail.com>
2014-01-08 18:44:34 +0530

    set to modern style UI

--- in.BBAT.presenter/src/in/bbat/presenter/ApplicationWorkbenchAdvisor.java
+++ in.BBAT.presenter/src/in/bbat/presenter/ApplicationWorkbenchAdvisor.java
@@ -5,6 +5,8 @@ import in.bbat.p2.rcpupdate.utils.P2Util;
 import in.bbat.presenter.perstpectives.DeveloperPerspective;
 
 import org.apache.log4j.Logger;
+import org.eclipse.ui.IWorkbenchPreferenceConstants;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchAdvisor;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
@@ -14,6 +16,7 @@ public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
 	private static final Logger LOG = BBATLogger.getLogger(ApplicationWorkbenchAdvisor.class.getName());
 	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(
 			IWorkbenchWindowConfigurer configurer) {
+		PlatformUI.getPreferenceStore().setValue(IWorkbenchPreferenceConstants.SHOW_TRADITIONAL_STYLE_TABS, false);
 		return new ApplicationWorkbenchWindowAdvisor(configurer);
 	}
 
