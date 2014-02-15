3ef4240514b30a90999f937939a6ddcf5d6be0a0
Syed Mehtab <mehtab.syed@gmail.com>
2014-02-15 22:59:27 +0530

    added comments

--- in.BBAT.TestRunner/src/in/BBAT/TestRunner/Listener/IAndroidDevice.java
+++ in.BBAT.TestRunner/src/in/BBAT/TestRunner/Listener/IAndroidDevice.java
@@ -10,45 +10,122 @@ import com.android.ddmlib.IDevice;
 import com.android.ddmlib.RawImage;
 import com.android.ddmlib.testrunner.ITestRunListener;
 
-
+/**
+ */
 public interface IAndroidDevice {
 
+	/**
+	 * 
+	 * @return
+	 */
 	String getModelName();
 
+	/**
+	 * 
+	 * @return
+	 */
 	String getDeviceId();
 
+	/**
+	 * 
+	 * @return
+	 */
 	String getStatus();
 
+	/**
+	 * 
+	 * @param listener
+	 */
 	void setLogListener(ILogListener listener);
 
+	/**
+	 * 
+	 */
 	void startLogging();
 
+	/**
+	 * 
+	 */
 	void stopLogging();
 
+	/**
+	 * 
+	 */
 	void activate() ;
 
+	/**
+	 * 
+	 */
 	void deActivate() ;
 
+	/**
+	 * 
+	 * @param jar
+	 */
 	void pushTestJar(UiAutoTestCaseJar jar);
 
+	/**
+	 * 
+	 * @param testCaseName
+	 * @param uiAutoListener
+	 * @param memoryListener
+	 * @param cpuListener
+	 * @param listener
+	 */
 	void executeTestCase(String testCaseName,IUiAutomatorListener uiAutoListener,IMemoryUsageListener memoryListener,ICpuUsageListener cpuListener,ITestRunListener... listener);
 
+	/**
+	 * returns device name
+	 * @return
+	 */
 	String getName();
 
+	/**
+	 * 
+	 * @return screenshot
+	 */
 	RawImage getScreenshot();
-	
+
+	/**
+	 * 
+	 * @return
+	 */
 	IDevice getMonkeyDevice();
 
+	/**
+	 * 
+	 * @return
+	 */
 	String getSerialNo();
-
+	/**
+	 * 
+	 * @return
+	 */
 	int getApiLevel();
-	
+
+	/**
+	 * 
+	 * @return
+	 */
 	boolean isUIAutomatorSupported();
 
+	/**
+	 * 
+	 * @return
+	 */
 	Map<String, String> getProperties();
-	
+
+	/**
+	 * 
+	 * @param property
+	 * @return
+	 */
 	String getPropertyValue(String property);
-	
+
+	/**
+	 * 
+	 * @return
+	 */
 	IChimpDevice getChimpDevice();
 
 	/**
@@ -57,14 +134,30 @@ public interface IAndroidDevice {
 	 */
 	void installPackage(String apkPath)throws Exception;
 
+	/**
+	 * 
+	 * @param listener
+	 */
 	void setScreenShotListener(IScreenShotListener listener);
-	
+
+	/**
+	 * 
+	 * @return
+	 */
 	IScreenShotListener getScreenShotListener();
-	
+
+	/**
+	 * 
+	 */
 	public void removeUIAutomatorJar();
-	
+
+	/**
+	 * 
+	 */
 	public void startMemoryUsageThread();
-	
-	
+
+	/**
+	 * 
+	 */
 	public void startCpuUsageThread();
 }
