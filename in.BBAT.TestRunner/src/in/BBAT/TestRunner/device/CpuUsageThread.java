14daafab6b3e39cf2cda520e6114525365813752
Syed Mehtab <mehtab.syed@gmail.com>
2014-02-16 10:53:09 +0530

    added definition

--- in.BBAT.TestRunner/src/in/BBAT/TestRunner/device/CpuUsageThread.java
+++ in.BBAT.TestRunner/src/in/BBAT/TestRunner/device/CpuUsageThread.java
@@ -1,25 +1,61 @@
 package in.BBAT.TestRunner.device;
 
+import java.io.IOException;
+
+import org.apache.log4j.Logger;
+
+import com.android.ddmlib.AdbCommandRejectedException;
+import com.android.ddmlib.MultiLineReceiver;
+import com.android.ddmlib.NullOutputReceiver;
+import com.android.ddmlib.ShellCommandUnresponsiveException;
+import com.android.ddmlib.TimeoutException;
+
 import in.BBAT.TestRunner.Listener.ICpuUsageListener;
+import in.BBAT.testRunner.runner.UiAutoTestCaseJar;
+import in.bbat.logger.BBATLogger;
 
 public class CpuUsageThread implements Runnable {
 
-	
+
 	private TestDevice device;
 	private ICpuUsageListener listener;
+	
+	private static final Logger LOG = BBATLogger.getLogger(CpuUsageThread.class.getName());
 
 	public CpuUsageThread(TestDevice device, ICpuUsageListener listener) {
 		this.device = device;
 		this.listener = listener;
 	}
+
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+
+		final String cmd ="dumsys cpuinfo";
+		try {
+			device.getMonkeyDevice().executeShellCommand(cmd, new MultiLineReceiver() {
+				@Override
+				public boolean isCancelled() {
+					return false;
+				}
+
+				@Override
+				public void processNewLines(String[] arg0) {
+
+				}
+			},0);
+		} catch (TimeoutException e) {
+			LOG.error(e);
+		} catch (AdbCommandRejectedException e) {
+			LOG.error(e);
+		} catch (ShellCommandUnresponsiveException e) {
+			LOG.error(e);
+		} catch (IOException e) {
+			LOG.error(e);
+		}
 	}
-	
+
 	public void stop(){
-		
+
 	}
 
 }
--- in.BBAT.TestRunner/src/in/BBAT/TestRunner/device/MemoryUsageThread.java
+++ in.BBAT.TestRunner/src/in/BBAT/TestRunner/device/MemoryUsageThread.java
@@ -1,11 +1,22 @@
 package in.BBAT.TestRunner.device;
 
+import java.io.IOException;
+
+import org.apache.log4j.Logger;
+
+import com.android.ddmlib.AdbCommandRejectedException;
+import com.android.ddmlib.MultiLineReceiver;
+import com.android.ddmlib.ShellCommandUnresponsiveException;
+import com.android.ddmlib.TimeoutException;
+
 import in.BBAT.TestRunner.Listener.IMemoryUsageListener;
+import in.bbat.logger.BBATLogger;
 
 public class MemoryUsageThread implements Runnable {
 
 	private TestDevice device;
 	private IMemoryUsageListener listener;
+	private static final Logger LOG = BBATLogger.getLogger(MemoryUsageThread.class.getName());
 
 	public MemoryUsageThread(TestDevice device, IMemoryUsageListener listener) {
 		this.device = device;
@@ -14,6 +25,29 @@ public class MemoryUsageThread implements Runnable {
 
 	@Override
 	public void run() {
+
+		final String cmd ="dumsys meminfo";
+		try {
+			device.getMonkeyDevice().executeShellCommand(cmd, new MultiLineReceiver() {
+				@Override
+				public boolean isCancelled() {
+					return false;
+				}
+
+				@Override
+				public void processNewLines(String[] arg0) {
+
+				}
+			},0);
+		} catch (TimeoutException e) {
+			LOG.error(e);
+		} catch (AdbCommandRejectedException e) {
+			LOG.error(e);
+		} catch (ShellCommandUnresponsiveException e) {
+			LOG.error(e);
+		} catch (IOException e) {
+			LOG.error(e);
+		}
 
 	}
 
