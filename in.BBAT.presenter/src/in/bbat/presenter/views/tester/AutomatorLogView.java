b24e3c1ea1f357e586480fdd8da1ee052808320f
Syed Mehtab <mehtab.syed@gmail.com>
2014-03-31 13:23:10 +0530

    save uiautomator logs

--- in.BBAT.presenter/src/in/bbat/presenter/views/tester/AutomatorLogView.java
+++ in.BBAT.presenter/src/in/bbat/presenter/views/tester/AutomatorLogView.java
@@ -1,5 +1,10 @@
 package in.bbat.presenter.views.tester;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
 import in.BBAT.abstrakt.presenter.run.model.AutomatorLogModel;
 import in.BBAT.abstrakt.presenter.run.model.TestRunInstanceModel;
 import in.BBAT.abstrakt.presenter.run.model.TestStatus;
@@ -8,6 +13,7 @@ import in.bbat.logger.BBATLogger;
 import in.bbat.presenter.views.BBATViewPart;
 
 import org.apache.log4j.Logger;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.layout.TableColumnLayout;
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.ColumnWeightData;
@@ -21,12 +27,22 @@ import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.MouseMoveListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.DirectoryDialog;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.ToolBar;
+import org.eclipse.swt.widgets.ToolItem;
+
+import com.android.ddmlib.logcat.LogCatMessage;
+import com.android.ddmuilib.ImageLoader;
 
 public class AutomatorLogView extends BBATViewPart {
 
@@ -36,6 +52,9 @@ public class AutomatorLogView extends BBATViewPart {
 	private AutoLogFilter filter;
 
 	private Text searchText;
+	private TestRunInstanceModel selectedModel;
+
+	private static final String IMAGE_SAVE_LOG_TO_FILE = "save.png";
 
 	public static final String ID= "in.BBAT.presenter.tester.AutoLogView";
 	@Override
@@ -81,7 +100,7 @@ public class AutomatorLogView extends BBATViewPart {
 
 	private void createTextFilter(Composite parent) {
 		Composite comp1 = new Composite(parent, SWT.BORDER);
-		comp1.setLayout(new GridLayout(1,false));
+		comp1.setLayout(new GridLayout(2,false));
 		comp1.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 
 		searchText = new Text(comp1, SWT.BORDER);
@@ -100,7 +119,53 @@ public class AutomatorLogView extends BBATViewPart {
 			}
 		});
 
+		ToolBar toolBar = new ToolBar(comp1, SWT.FLAT);
+
+		ToolItem saveToLog = new ToolItem(toolBar, SWT.PUSH);
+		saveToLog.setImage(ImageLoader.getDdmUiLibLoader().loadImage(IMAGE_SAVE_LOG_TO_FILE,toolBar.getDisplay()));
+		saveToLog.setToolTipText("Export Selected Items To Text File..");
+		saveToLog.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent arg0) {
+				saveLogToFile();
+			}
+		});
+
 	}
+
+	/**
+	 * Save logcat messages selected in the table to a file.
+	 */
+	private void saveLogToFile() {
+		/* show dialog box and get target file name */
+		final String fName = getLogFileTargetLocation();
+		if (fName == null) {
+			return;
+		}
+		
+		try {
+			selectedModel.exportUIAutoLogs(fName);
+		} catch (IOException e) {
+			LOG.error(e);
+		}
+
+	}
+
+	/**
+	 * Display a {@link FileDialog} to the user and obtain the location for the log file.
+	 * @return path to target file, null if user canceled the dialog
+	 */
+	private String getLogFileTargetLocation() {
+		DirectoryDialog fd = new DirectoryDialog(Display.getCurrent().getActiveShell(), SWT.SAVE);
+
+		fd.setText("Save Log..");
+
+		fd.setFilterPath(System.getProperty("user.home"));
+
+		String fName = fd.open();
+		return fName;
+	}
+
 
 	public void createColumns(final Composite parent, final TableViewer viewer) {
 		String[] titles = { "Message" };
@@ -132,6 +197,7 @@ public class AutomatorLogView extends BBATViewPart {
 		//		if(model.getStatus().equalsIgnoreCase(TestStatus.EXECUTING.getStatus()))
 		/*viewer.setInput(model.getAutoLogs());
 		else*/
+		selectedModel = model;
 		viewer.setInput(model.getAutoLogsFromDB());
 		setPartName("UiAutomator Logs: "+model.getTestCaseEntity().getName());
 
